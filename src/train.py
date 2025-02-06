4306123ebe62463fdc4e6f31831c833ded08681d
xowski22 <xowski22@gmail.com>
2025-02-06 18:00:34 +0100

    Changed training script to better suit new data preprocessing

--- src/train.py
+++ src/train.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 from data.preprocessing import load_ml1m_data, preprocess_ratings, split_data
 from data.dataset import RecommenderDataset
+from src.api.app import user_mapping
+from src.data.loaders.loader_factory import create_train_val_dataloaders
 from src.models.model import MatrixFactorization
 from src.training.trainer import train_model
 
@@ -18,33 +20,21 @@ def main():
     (ROOT_DIR / 'models' / 'checkpoints').mkdir(parents=True, exist_ok=True)
 
     config_path = ROOT_DIR / 'config' / "config.yaml"
-    data_path = ROOT_DIR / 'data' / 'raw' / 'ml-1m'
-    mappings_dir = ROOT_DIR / 'data' / 'mappings'
 
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
 
-    ratings_df, _ = load_ml1m_data(data_path)
-    processed_df, user_mapping, item_mapping = preprocess_ratings(ratings_df)
-    train_data, val_data = split_data(processed_df)
+    config['data']['path'] = str(ROOT_DIR / 'data' / 'raw' / 'ml-1m')
+    config['data']['mappings_path'] = str(ROOT_DIR / 'data' / 'mappings')
 
-    train_dataset = RecommenderDataset(train_data)
-    val_dataset = RecommenderDataset(val_data)
+    train_loader, val_loader, user_mapping, item_mapping = create_train_val_dataloaders(config)
 
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=config['training']['batch_size'],
-        shuffle=True)
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=config['training']['batch_size'],
-        shuffle=True)
-
-    model = MatrixFactorization(num_users=len(user_mapping),
-                                n_items=len(item_mapping),
-                                embedding_dim=config['model']['embedding_dim'],
-                                reg_lambda=config['model']['reg_lambda']
-                                )
+    model = MatrixFactorization(
+        num_users=len(user_mapping),
+        n_items=len(item_mapping),
+        embedding_dim=config['model']['embedding_dim'],
+        reg_lambda=config['model']['reg_lambda']
+    )
 
     trained_model = train_model(model, train_loader, val_loader, config['training'])
 
